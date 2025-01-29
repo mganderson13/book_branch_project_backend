a3438fb4f639f3c19e7780115c17264a1e7b3be4
Marianne Anderson <mganderson13@gmail.com>
2025-01-28 21:10:03 -0600

    mongoDB installation and set up

--- src/server.js
+++ src/server.js
@@ -1,6 +1,47 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import { MongoClient } from 'mongodb';
 
 const app = express();
+app.use(express.json());
+
+const uri = process.env.MONGODB_URI
+console.log("MongoDB URI:", process.env.MONGODB_URI);
+const client = new MongoClient(uri);
+
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+app.put('/api/details/:id/like', (req, res) => {
+    const { id } = req.params;
+    const book = bookInfo.find(b => b.id === id);
+    if (book) {
+    book.like = true;
+    }
+});
+
+app.post('api/details/:id/review', (req, res) => {
+    const { id } = req.params;
+    const { postedBy, review } = req.body;
+
+    const book = bookInfo.find(b => b.id === id);
+    if (book) {
+        
+    }
+});
 
 app.listen(8000, () => {
     console.log('Server is listening on port 8000');
